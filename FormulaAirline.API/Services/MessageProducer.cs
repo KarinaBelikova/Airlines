d895fffe2255bf462ecc919e8d31a97a869feb60
Karina Belikova <kiit15.belikova@gmail.com>
2023-08-08 21:46:45 +0300

    Initial commit

---
+++ FormulaAirline.API/Services/MessageProducer.cs
@@ -0,0 +1,27 @@
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace FormulaAirline.API.Services;
+
+public class MessageProducer : IMessageProducer
+{
+    public void SendingMessage<T>(T message)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            UserName = "user",
+            Password = "mypass",
+            VirtualHost = "/", 
+        };
+
+        var conn = factory.CreateConnection();
+        using var chanel = conn.CreateModel();
+        chanel.QueueDeclare("bookings", durable: true, exclusive: true);
+        var jsonString = JsonSerializer.Serialize(message);
+        var body = Encoding.UTF8.GetBytes(jsonString);
+
+        chanel.BasicPublish("", "bookings", body: body);
+    }
+}
